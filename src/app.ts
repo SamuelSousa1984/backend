16859b990e697544d25c4e939a57858a167adc2d
Samuel Sousa <samuelsousapereira@gmail.com>
2023-09-23 15:03:29 -0300

    CORS Origin false

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-    origin: '*',
+    origin: false,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     'preflightContinue': false,
     'optionsSuccessStatus': 204
