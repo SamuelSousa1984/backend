3f4b9cee9442fcf26a1c673bfe41a14168741a06
Samuel Sousa <samuelsousapereira@gmail.com>
2023-09-23 18:55:36 -0300

    Restaurando configuração inicial

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,7 @@ import routes from './routes/routes';
 require('dotenv').config();
 
 const app = express();
-app.use(cors({
-    origin: false,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'preflightContinue': false,
-    'optionsSuccessStatus': 204
-}))
+app.use(cors())
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.DATABASE_URL || '';
 
