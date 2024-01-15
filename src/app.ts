08fdac8f7a7ab459c94432af0084cdf5720a6b99
Samuel Sousa <samuelsousapereira@gmail.com>
2024-01-15 13:37:56 -0300

    Adicionando Nodemon ambiente dev

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(helmet({
 app.use(cors({
     origin: '*' || 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
+    allowedHeaders: 'Content-Type, Authorization',
     credentials: false
 }));
 
