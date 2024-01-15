08e71f4c3ca360b1e4408fbb421408f3680af163
Samuel Sousa <samuelsousapereira@gmail.com>
2024-01-15 14:38:16 -0300

    CORS test 5

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -2,11 +2,6 @@ import {Request, Response} from 'express';
 import Model, { Convidados } from '../models/model';
 
 export const postController = async (req:Request, res:Response) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
     const { nome, idade } = req.body;
 
     try {
